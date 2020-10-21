1f457002fc3fc69ac7dde240bd9b8aa1d8a3a774
Karan Shah <karanshah343@gmail.com>
2020-10-21 19:39:06 -0400

    configured react-hot-reload for hot reloading

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
-import "./App.css";
+import { hot } from 'react-hot-loader';
+
 import Header from "../src/compenents/header/Header";
 import Footer from "../src/compenents/footer/Footer";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <p>Hello WOeld</p>
+      <p>Hello World!</p>
       <Footer />
     </div>
   );
 }
 
-export default App;
+export default hot(module)(App);
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,37 @@
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://0.0.0.0:3000',
+    'webpack/hot/only-dev-server',
+    './src/index',
+  ],
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 3000,
+    publicPath: '/',
+    historyApiFallback: true,
+    disableHostCheck: true
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'app.[hash].js'
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+        loader: "babel-loader",
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['react-hot-loader/babel']
         }
       },
       {
