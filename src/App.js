8e3425fb917e1d94c48e52962f569594536877b4
Karan Shah <karanshah343@gmail.com>
2020-10-21 20:07:31 -0400

    fixed css issues and restructured code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from "react";
-import { Container } from "reactstrap";
 import { hot } from 'react-hot-loader';
 
-import Header from "../src/compenents/header/Header";
-import Footer from "../src/compenents/footer/Footer";
+import Header from "../src/compenents/header";
+import Footer from "../src/compenents/footer";
 import Banner from "./compenents/banner";
 
 import "./App.css";
 
 function App() {
   return (
-    <Container fluid className="App bg-dark">
+    <div className="App bg-dark">
       <Header />
       <Banner />
       <Footer />
-    </Container>
+    </div>
   );
 }
 
---
+++ src/compenents/header/index.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  InputGroup,
+  Input,
+  Button,
+} from "reactstrap";
+
+const Header = (props) => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <Navbar color="light" light expand="md">
+      <NavbarBrand>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/underground/iron-ball.png"
+          alt="..."
+          className="rounded-circle"
+        />
+      </NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <div className='d-flex justify-content-between w-100'>
+          <div>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="/components/">PokeWiki</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Pokemon</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Pokemon Type
+              </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Water</DropdownItem>
+                  <DropdownItem>Fire</DropdownItem>
+                  <DropdownItem>Air</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </div>
+          <div>
+            <InputGroup>
+              <Input></Input>
+              <Button href="#" className="ml-2">
+                Search
+            </Button>
+            </InputGroup>
+          </div>
+        </div>
+      </Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
