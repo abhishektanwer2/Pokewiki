7fc77fdc9b6f1334c1ff45195d15a30c27dc0c88
Abhishek Tanwer <abhishektanwer2@gmail.com>
2020-10-30 12:46:48 -0400

    updating

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { hot } from "react-hot-loader";
+import Hea
 
 import HomePage from "./pages/homepage";
 import Header from "../src/compenents/header";
 import Banner from "./compenents/banner";
+<<<<<<< HEAD
 import Card from "./compenents/card";
+=======
+import Card from './compenents/card';
+>>>>>>> updating
 import Footer from "../src/compenents/footer";
 import Pokedetails from "../src/compenents/pokedetails";
 
@@ -14,6 +19,7 @@ import "./styles/custom.css";
 function App() {
   return (
     <div className="App bg-dark text-white">
+<<<<<<< HEAD
       <Header />
       <Banner />
       <div>
@@ -21,6 +27,9 @@ function App() {
       </div>
       <div>{/* <HorizontalCards /> */}</div>
       <Footer />
+=======
+      <Header></Header>
+>>>>>>> updating
     </div>
   );
 }
--- src/compenents/horizontalCards/index.js
+++ src/compenents/horizontalCards/index.js
@@ -1,12 +1,52 @@
+<<<<<<< HEAD
 import React, { useState } from "react";
 import { Card } from "reactstrap";
+=======
+import React from "react";
+import { Card } from "reactstrap";
+import React, { useState } from "react";
+import { Card } from "reactstrap";
+>>>>>>> updating
 
 import "./style.css";
 
 const HorizontalCards = (props) => {
+<<<<<<< HEAD
   const { pokemons } = props;
   const IMAGE_URL = process.env.IMAGE_URL;
   if (pokemons.length > 0) {
+=======
+  const {
+    data = [
+      {
+        image:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+      },
+      {
+        image:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        image:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        image:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+      },
+      {
+        image:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        image:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+    ],
+  } = props;
+
+  if (data.length > 0) {
+>>>>>>> updating
     return (
       <div className="cardsContainer py-3">
         {pokemons.map((pokemonData, index) => {
@@ -29,11 +69,44 @@ const HorizontalCards = (props) => {
       </div>
     );
   }
+<<<<<<< HEAD
   return (
     <div>
       <p>Something went wrong</p>
     </div>
   );
 };
+=======
+  return <div></div>;
+};
+const { pokemons } = props;
+const IMAGE_URL = process.env.IMAGE_URL;
+if (pokemons.length > 0) {
+  return (
+    <div className="cardsContainer py-3">
+      {pokemons.map((pokemonData, index) => {
+        const { url } = pokemonData.pokemon;
+        return (
+          <div key={index} className="pokemonCard">
+            <Card className="box">
+              <img
+                src={
+                  IMAGE_URL + url.split("/")[url.split("/").length - 2] + ".png"
+                }
+                className="w-100"
+              ></img>
+            </Card>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+return (
+  <div>
+    <p>Something went wrong</p>
+  </div>
+);
+>>>>>>> updating
 
 export default HorizontalCards;
