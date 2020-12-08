182c015589825e3ac240a562624b3a93e1ddef43
Karan Shah <karanshah343@gmail.com>
2020-12-08 00:21:34 -0500

    resolved production build env issue

--- webpack.prod.js
+++ webpack.prod.js
@@ -18,6 +18,9 @@ module.exports = merge(common, {
       filename: 'styles/[name].[contenthash].css',
       chunkFilename: '[id].css',
     }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
   ],
   optimization: {
     minimize: true,
