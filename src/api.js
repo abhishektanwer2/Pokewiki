d699b867a6d8b3583fb41a3cc8e8bcc9472bdcb1
Karan Shah <karanshah343@gmail.com>
2020-12-09 00:28:48 -0500

    test

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_URL,
-  timeout: 10000,
-  headers: {
+  "baseURL": process.env.REACT_APP_URL,
+  "timeout": 5000,
+  "headers": {
     "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
     "X-Custom-Header": "foobar"
   }
