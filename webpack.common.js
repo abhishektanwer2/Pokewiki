5d676bc6e789c693106f2d30f12069fd23d312bc
Karan Shah <karanshah343@gmail.com>
2020-12-09 01:28:28 -0500

    class properties plugin test

--- webpack.common.js
+++ webpack.common.js
@@ -12,7 +12,15 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: ['babel-loader'] },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["react-hot-loader/babel", "@babel/plugin-proposal-class-properties"],
+        },
+      },
       {
         test: /\.(scss|css)$/,
         use: [
